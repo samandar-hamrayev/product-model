b6d02c0764102846793e445d618344befc271a23
hamrayev7 <samandarhamrayev555@gmail.com>
2025-02-22 10:06:20 +0500

    imagelar korinmayotgan edi, urls.py fayllarini sozladim, juda kichik ozgarish

--- products/urls.py
+++ products/urls.py
@@ -8,8 +8,3 @@ urlpatterns = [
     path('', product_list, name='product_list'),
     path('<int:product_id>/', product_detail, name='product_detail'),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
